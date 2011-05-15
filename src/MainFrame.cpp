64ee70cf56eae3478001bf95ffb28466e1c7fd2b
x-andre <x-andre@71f4f73e-3029-49f6-977e-f6bbc94eda0c>
2011-05-15 17:25:57 +0000

    Added the beta suffix to the version

--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -63,7 +63,7 @@ QPointer<MainFrame> MainFrame::_self = 0L;
 MainFrame::MainFrame(QWidget *parent, Qt::WFlags flags)
 	: QMainWindow(parent, flags)
 {
-	m_VersionName = QString::fromLatin1("XFLR5 v6.04");
+	m_VersionName = QString::fromLatin1("XFLR5 v6.04 Beta");
 	QString jpegPluginPath;
 
 	//Jpeg format requires a specific plugin to be loaded dynmically at run time
